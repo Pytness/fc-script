fabad209b85f6721b4825a3cf15b899e823ec815
Fernando Vega <pytness@gmail.com>
2018-08-08 02:21:06 +0200

    Added update support

--- src/iconAutocomplete/index.js
+++ src/iconAutocomplete/index.js
@@ -8,6 +8,7 @@
 // @match        https://www.forocoches.com/foro/newreply.php*
 // @match        https://www.forocoches.com/foro/private.php*
 // @resource     iconsJson https://raw.githubusercontent.com/Pytness/fc-script/master/src/iconAutocomplete/icons.json
+// @updateURL	 https://raw.githubusercontent.com/Pytness/fc-script/master/src/iconAutocomplete/index.js
 // @run-at       document-end
 // @grant        GM_getResourceText
 // ==/UserScript==
