6633f3760b20bdef484944965ee45a0020ea7188
Fernando Vega Catalán <pytness@gmail.com>
2018-11-06 13:17:17 +0100

    Changed version

--- src/viewOpComments/index.user.js
+++ src/viewOpComments/index.user.js
@@ -2,7 +2,7 @@
 // @name         viewOpComments
 // @description  Este script elimina los posts que no sean del OP
 // @author       pytness
-// @version      1.0
+// @version      1.2
 // @namespace    http://tampermonkey.net/
 // @match        https://www.forocoches.com/foro/showthread.php*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
