dd6622b7d654da117bde3d13fb24e4c49f2252b4
Fernando Vega Catalán <pytness@gmail.com>
2022-06-26 16:51:36 +0200

    Pump up version

--- src/deleteIgnoredUsersPosts/index.user.js
+++ src/deleteIgnoredUsersPosts/index.user.js
@@ -2,7 +2,7 @@
 // @name         Delete Ignored Users Posts
 // @description  Deletes posts if its creator its in your ignored list (@zaguarman & @Papademos69)
 // @author       Pytness
-// @version      1.06
+// @version      1.07
 // @namespace    http://tampermonkey.net/
 // @match        https://forocoches.com/
 // @match        https://forocoches.com/foro/forumdisplay.php?f=*
